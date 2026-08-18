db360a31c8eac3f3031a072ed963da26e0231c28
rusofen <53075225+rusofen@users.noreply.github.com>
2026-08-18 20:51:19 +0300

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
